44a25ce5a980ce085defd28ffdf504ee86560cc9
pdLeal <lealpd@outlook.com>
2023-11-01 03:25:30 -0300

    feat: displayValue(old getValue) function completed   also changed values on some buttons

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function getValue() {
-    console.log(this.value);
+const btns = document.querySelectorAll("button");
+const display = document.getElementById("display");
+
+function displayValue() {
+    const regexForNum = /\d/;
+    const regexForOperator = /[x÷+-]/;
+
+
+    if (regexForNum.test(this.value)) {
+
+        if (display.textContent === "0") {
+            display.textContent = "";
+        }
+
+        display.textContent += this.value;
+
+    } else if (regexForOperator.test(this.value) && display.textContent !== "0") {
+        const checkOperator = display.textContent[display.textContent.length - 2];
+
+        if (!checkOperator || !regexForOperator.test(checkOperator)) {
+            display.textContent += ` ${this.value} `;
+        }
+    }
+
 }
 
-const btns = document.querySelectorAll("button")
-btns.forEach(btn => btn.addEventListener("click", getValue));
+btns.forEach(btn => btn.addEventListener("click", displayValue));
