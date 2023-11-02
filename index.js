7824f8de90afb0eba251aa888f41b347e31b2754
pdLeal <lealpd@outlook.com>
2023-11-02 20:54:50 -0300

    fix: bug on fixPriority   was indexOfMulti === -1 istead of indexOfMult === -1

--- index.js
+++ index.js
@@ -105,20 +105,18 @@ function fixPriority(array) {
         copyArray.splice(indexOfMult - 1, 3, splitSlice);
 
 
-        return [...fixPriority(copyArray)]
-    } else if ((indexOfMult > indexOfDiv || indexOfMulti === -1) && indexOfDiv>= 0) {
+        return fixPriority(copyArray)
+    } else if ((indexOfMult > indexOfDiv || indexOfMult === -1) && indexOfDiv>= 0) {
         const splitSlice = array.slice(indexOfDiv - 1, indexOfDiv + 2);
 
         copyArray.splice(indexOfDiv - 1, 3, splitSlice);
 
 
-        return [...fixPriority(copyArray)]
+        return fixPriority(copyArray)
     }
 }
 
-// console.log(fixOperators("8 * 3 - 1 + 2 * 5 / 2 - 4 * 3"));
-
-console.log(fixPriority(fixOperators("8 * 3 - 1 + 2 * 5 / 2 - 4 * 3")));  // 16
+// console.log(fixPriority(fixOperators("8 * 3 - 1 + 2 * 5 / 2 - 4 * 3")));  // 16
 
 btns.forEach(btn => btn.addEventListener("click", displayValue));
 // equal.addEventListener("click", operate);
