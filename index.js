a53b5fe63c477aad8866d290c35b7d8d0500cbc2
pdLeal <lealpd@outlook.com>
2023-11-02 22:09:04 -0300

    add: .map to fixOperators   also started to work on operate function

--- index.js
+++ index.js
@@ -25,70 +25,46 @@ function displayValue() {
 
 }
 
-// function operate() {
+function operate() {
+    const fixedOperators = fixPriority(fixOperators("8 * 3 - 1 + 2 * 5 / 2 - 4 * 3"));
+    const rightPriority = fixPriority(fixedOperators);
 
-//     
-//     console.log("splitStr antes: ", splitStr);
+    console.log(rightPriority);
 
-//     const copySplit = [...splitStr];
+    function map(array) {
+        return array.map(elem => {
 
-//     // const test = splitStr.reduce((prev, curr, index, arr) => {
+            if (!Array.isArray(elem)) {
+                return elem
+            } else if(!Array.isArray(elem[0])) {
+                switch (elem[1]) {
+                    case "*":
+                        return elem[0] * elem[2]
+                        break;
+                
+                    default:
+                        break;
+                }
+            } else {
+                return map(elem);
+            }
+        })
 
-//     //     //     if (/[*/+-]/.test(curr)) {
-//     //     //         switch (curr) {
-//     //     //             case "+":
-//     //     //                 return prev + Number(arr[index + 1]);
-//     //     //                 break;
-//     //     //             case "-":
-//     //     //                 return prev - Number(arr[index + 1]);
-//     //     //                 break;
-//     //     //             case "*":
-//     //     //                 return prev * Number(arr[index + 1]);
-//     //     //                 break;
-//     //     //             case "/":
-//     //     //                 return prev / Number(arr[index + 1]);
-//     //     //         }
-//     //     //     } else {
-//     //     //         return prev;
-//     //     //     }
-//     //     // }, Number(splitStr[0])));
+    }
 
-//     //     if (/[*/]/.test(curr)) {
-//     //         switch (curr) {
-//     //             case "*":
-
-//     //                 console.table(copySplit);
-
-//     //                 const currIndex = copySplit.indexOf(curr);
-
-//     //                 const prevIndex = currIndex - 1;
-//     //                 const nextIndex = currIndex + 1;
-
-//     //                 console.table("Depois: ", copySplit);
-
-//     //                 console.log(`prevIndex: ${prevIndex}, currIndex: ${currIndex}, nextIndex: ${nextIndex}`);
-
-//     //                 return Number(prev) * Number(arr[index + 1]);
-//     //                 break;
-//     //             case "/":
-//     //                 return prev / Number(arr[index + 1]);
-//     //         }
-//     //     } else {
-//     //         return prev;
-//     //     }
-//     // }, splitStr[0]);
-
-//     // console.log(`The result is: ${test}`);
-// }
+    console.log(map(rightPriority));
+}
+operate();
 
 function fixOperators(str) {
-    const valueDisplayed = display.textContent; // retirar parametros e trocar por valueDisplayed após testes
-    const fixedStr = str.replace(/x|÷/g, match => {
-        return match === 'x' ? '*' : '/';
-    });
-    const splitStr = fixedStr.split(" ");
+    const valueDisplayed = display.textContent; // retirar str e trocar por valueDisplayed após testes
 
-    return splitStr;
+    return str.replace(/x|÷/g, match => {
+        return match === 'x' ? '*' : '/';
+    }).split(" ").map(elem => {
+        const regex = /[*/+-]/;
+        return regex.test(elem) ? elem : +elem;
+    });
 }
 
 function fixPriority(array) {
@@ -106,7 +82,7 @@ function fixPriority(array) {
 
 
         return fixPriority(copyArray)
-    } else if ((indexOfMult > indexOfDiv || indexOfMult === -1) && indexOfDiv>= 0) {
+    } else if ((indexOfMult > indexOfDiv || indexOfMult === -1) && indexOfDiv >= 0) {
         const splitSlice = array.slice(indexOfDiv - 1, indexOfDiv + 2);
 
         copyArray.splice(indexOfDiv - 1, 3, splitSlice);
@@ -119,4 +95,4 @@ function fixPriority(array) {
 // console.log(fixPriority(fixOperators("8 * 3 - 1 + 2 * 5 / 2 - 4 * 3")));  // 16
 
 btns.forEach(btn => btn.addEventListener("click", displayValue));
-// equal.addEventListener("click", operate);
+equal.addEventListener("click", operate);
