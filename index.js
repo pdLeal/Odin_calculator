13c75a42dfb37a6c431118e2ceb17022524ca79c
pdLeal <lealpd@outlook.com>
2023-11-06 23:11:06 -0300

    operate function now makes multiplication and division correctly   insert meme of happines here!

--- index.js
+++ index.js
@@ -26,25 +26,32 @@ function displayValue() {
 }
 
 const smallOp = "2 + 5 * 5 / 8 * 3 - 1 + 2 * 5 / 2 - 4 * 3";
-const bigOp = "8 * 3 - 1 + 2 * 5 / 2 * 3 * 5 / 4 / 3 * 27 - 4 * 3";
+const bigOp = "8 * 3 - 1 + 2 * 5 / 2 * 3 * 5 / 4 / 3 * 10 - 4 * 3";
+const testOp = "5 + 2 * 2 * 2 * 2 * 2 + 5";
 
 function operate(str) {
-    
+
     const fixed = fixPriority(fixOperators(str));
-    console.log(fixed);
 
-    const newArr = [];
-
-    for(let i = 0; i < fixed.length; i++) {
-        if(Array.isArray(fixed[i])) {
-            newArr.push(fixed[i]);
+    const newArr = fixed.map(value => {
+        function test(value) {
+            if(Array.isArray(value)) {
+                if(value[1] === "*") {
+                    return test(value[0]) * value[2]
+                } else {
+                    return test(value[0]) / value [2]
+                }
+            } else {
+                return value;
+            }
         }
-    }
-    console.log(newArr);
+        return test(value)
+    })
 
+    console.log(newArr);
 }
 
-operate(smallOp);
+operate(bigOp);
 
 function fixOperators(str) {
     const valueDisplayed = display.textContent; // retirar str e trocar por valueDisplayed após testes
@@ -81,8 +88,6 @@ function fixPriority(array) {
         return fixPriority(copyArray)
     }
 }
-
-// console.log(fixPriority(fixOperators("8 * 3 - 1 + 2 * 5 / 2 - 4 * 3")));  // 16
 
 btns.forEach(btn => btn.addEventListener("click", displayValue));
 equal.addEventListener("click", operate);
